edeb7d95378961c037735a1a4091e86c272c07b1
tanmayswain11 <tanmayswain464@gmail.com>
2026-02-19 23:25:19 +0530

    change multer

--- src/Middlewire/upload.middleware.js
+++ src/Middlewire/upload.middleware.js
@@ -1,3 +1,39 @@
+// import multer from "multer";
+// import fs from "fs";
+// import path from "path";
+
+// // TEMP FOLDER
+// const uploadPath = path.join(process.cwd(), "public", "temt");
+
+// if (!fs.existsSync(uploadPath)) {
+//   fs.mkdirSync(uploadPath, { recursive: true });
+// }
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, uploadPath);
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
+
+// // ALLOW PDF + IMAGE
+// const fileFilter = (req, file, cb) => {
+//   if (
+//     file.mimetype === "application/pdf" ||
+//     file.mimetype.startsWith("image/")
+//   ) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error("Only PDF & Image allowed"), false);
+//   }
+// };
+
+// const upload = multer({ storage, fileFilter });
+
+// export default upload;
+
 import multer from "multer";
 import fs from "fs";
 import path from "path";
@@ -30,6 +66,12 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-const upload = multer({ storage, fileFilter });
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 50 * 1024 * 1024, // ✅ 50MB
+  },
+});
 
 export default upload;
